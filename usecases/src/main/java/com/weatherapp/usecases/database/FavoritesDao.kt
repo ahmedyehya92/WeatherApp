d931215c43f29430900a6c9f3b385e5b27bfaca7
Ahmed Yehya <ahmedyehya1992@gmail.com>
2019-03-19 22:08:53 +0200

    test

---
+++ usecases/src/main/java/com/weatherapp/usecases/database/FavoritesDao.kt
@@ -0,0 +1,10 @@
+package com.weatherapp.usecases.database
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Query
+
+@Dao
+interface FavoritesDao{
+    @Query("select * from FavoriteCityId")
+    fun queryAll()
+}
