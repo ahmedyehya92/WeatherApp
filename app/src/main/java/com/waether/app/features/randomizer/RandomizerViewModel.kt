717a715f7ba4ea47c5e5acd64c388fee87a48c69
Ahmed Yehya <ahmedyehya1992@gmail.com>
2019-03-19 14:48:35 +0200

    create develop branch

---
+++ app/src/main/java/com/waether/app/features/randomizer/RandomizerViewModel.kt
@@ -0,0 +1,35 @@
+package com.waether.app.features.randomizer
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.weatherapp.usecases.Ticker
+import com.weatherapp.usecases.randomNumberGenerator
+
+
+private const val DEFAULT_VALUE = 0
+
+class RandomizerViewModel : ViewModel() {
+
+    private val ticker = initializeTicker()
+    val numberLiveData = MutableLiveData<Int>()
+
+
+
+    init {
+        numberLiveData.value = DEFAULT_VALUE
+        ticker.start()
+    }
+
+
+
+
+    private fun initializeTicker() = Ticker {
+        numberLiveData.postValue(randomNumberGenerator().toInt())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        ticker.cancel()
+    }
+
+}
